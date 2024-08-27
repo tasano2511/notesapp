b675ef0606b2d03d889a68fd93393b30fe804d6c
sushi314@hotmail.com <sushi314@hotmail.com>
2024-08-27 14:00:59 -0400

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import "./App.css";
 import logotenniscourt from './image/tenniscourt.jpg';
 import logotenniscourt2 from './image/tenniscourt2.jpg';
 import logotenniscourt3 from './image/tenniscourt3.jpg';
+import SimpleImageSlider from "react-simple-image-slider";
+// import React, { Component, useEffect, useState} from "react";
 
 //image slider
-const [show, setShow] = useState(true)
+// const [show, setShow] = useState(true)
 const images = [
   { url: logotenniscourt },
   { url: logotenniscourt2 },
@@ -14,22 +16,21 @@ const images = [
 
 function App() {
 
-  <div className="App-div">
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-div">
             <SimpleImageSlider
                 width={400}
-                height={200}
+                height={600}
                 images={images}
                 showBullets={true}
                 showNavs={true}
             />
         </div>
-
-  return (
-    <div className="App">
-      <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
-
         <h1>Hello from Amplify</h1>
+        
       </header>
     </div>
   );
